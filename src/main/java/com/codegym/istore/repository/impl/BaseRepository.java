98d20581f0f57a475be352a2737b3b791ca742c1
daonguyendev <daonguyen.dev@gmail.com>
2024-07-05 15:41:20 +0700

    Refactor codees

--- src/main/java/com/codegym/istore/repository/impl/BaseRepository.java
+++ src/main/java/com/codegym/istore/repository/impl/BaseRepository.java
@@ -8,7 +8,7 @@ public class BaseRepository {
 
     private static String jdbcURL = "jdbc:mysql://localhost:3306/casestudy_module_3?useSSL=false";
     private static String jdbcUsername = "root";
-    private static String jdbcPassword = "0339198933ky";
+    private static String jdbcPassword = "root";
     private static Connection connection = null;
     static {
         try {
